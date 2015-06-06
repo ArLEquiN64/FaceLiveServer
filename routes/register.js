69255b51655359cee48e623b02dae21e9ec1f7c8
ArLEquiN64 <ARLEQUIN.prx@gmail.com>
2015-06-06 22:44:11 +0900

    add register

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var redis = require('redis');
+
+client = redis.createClient();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('register ' + req.query.user);
+router.post('/', function(req, res, next) {
+  var json = {name: req.query.name, mail: req.query.mail};
+  if(req.query.tell){json.mail = req.query.tell;}
+  if(req.query.twitter){json.twitter = req.query.twitter;}
+  client.set(req.query.id, json,function(err, keys_replies){
+    if(err){throw err;}
+    else{
+    res.OK(req.query.name + "is registered.");}
+  });
+  res.send("error occured");
 });
 
 module.exports = router;
