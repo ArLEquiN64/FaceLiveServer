478e77d468d68468cf8e343f75a477f967e0622e
ArLEquiN64 <ARLEQUIN.prx@gmail.com>
2015-06-07 06:20:37 +0900

    fix :anger:

--- routes/tweet.js
+++ routes/tweet.js
@@ -17,7 +17,7 @@ router.post('/', function(req, res, next) {
         time: dt.toFormat("YYYYMMDDHH24MI")
       };
       if(!json.name || !req.body.tweet){res.send("luck params");return;}
-      client.set(req.body.id, JSON.stringify(JSON.parse(reply).tl.push(json)), function(err, keys_replies){
+      client.set("fl-tweets", JSON.stringify(JSON.parse(reply).tl.unshift(json)), function(err, keys_replies){
         if(err){throw err;}
         else{res.send(req.body.name + " tweet.");return;}
         return;
