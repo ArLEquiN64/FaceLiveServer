9cd6524e9bfac89b2938619f0c8588a934be953c
ArLEquiN64 <ARLEQUIN.prx@gmail.com>
2015-06-06 23:00:47 +0900

    check params

--- routes/register.js
+++ routes/register.js
@@ -6,9 +6,13 @@ client = redis.createClient();
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  var json = {name: req.body.name, mail: req.body.mail};
-  if(req.body.tell){json.mail = req.body.tell;}
-  if(req.body.twitter){json.twitter = req.body.twitter;}
+  var json = {
+    name: req.body.name,
+    mail: req.body.mail,
+    tell: req.body.tell,
+    twitter: req.body.twitter
+  };
+  if(!json.mail || !json.name || !req.body.id){res.send("luck params");}
   client.set(req.body.id, JSON.stringify(json), function(err, keys_replies){
     if(err){throw err;}
     else{res.send(req.body.name + "is registered.");}
