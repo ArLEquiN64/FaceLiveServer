051d68df87686537438eab450be515c94eb34022
ArLEquiN64 <ARLEQUIN.prx@gmail.com>
2015-06-06 22:47:28 +0900

    bug fixed

--- routes/register.js
+++ routes/register.js
@@ -9,10 +9,9 @@ router.post('/', function(req, res, next) {
   var json = {name: req.query.name, mail: req.query.mail};
   if(req.query.tell){json.mail = req.query.tell;}
   if(req.query.twitter){json.twitter = req.query.twitter;}
-  client.set(req.query.id, json,function(err, keys_replies){
+  client.set(req.query.id, JSON.stringify(json), function(err, keys_replies){
     if(err){throw err;}
-    else{
-    res.OK(req.query.name + "is registered.");}
+    else{res.OK(req.query.name + "is registered.");}
   });
   res.send("error occured");
 });
