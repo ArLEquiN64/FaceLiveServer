e091098ba3de3be22ea45cc63be0b58e5d710728
ArLEquiN64 <ARLEQUIN.prx@gmail.com>
2015-06-06 21:20:25 +0900

    login test

---
+++ app.js
@@ -0,0 +1,92 @@
+var MongoStore = require('connect-mongo')(express);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.cookieParser()); //追加
+app.use(express.session({
+    secret: 'secret',
+    store: new MongoStore({
+        db: 'session',
+        host: 'localhost',
+        clear_interval: 60 * 60
+    }),
+    cookie: {
+        httpOnly: false,
+        maxAge: new Date(Date.now() + 60 * 60 * 1000)
+    }
+})); //追加
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var loginCheck = function(req, res, next) {
+    if(req.session.user){
+      next();
+    }else{
+      res.redirect('/login');
+    }
+};
+
+app.get('/', loginCheck, routes.index);
+app.get('/login', routes.login);
+app.post('/add', routes.add);
+app.get('/logout', function(req, res){
+  req.session.destroy();
+  console.log('deleted sesstion');
+  res.redirect('/');
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
