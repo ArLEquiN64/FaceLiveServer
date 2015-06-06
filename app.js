5f0da8e9a0d353dca486e0488b7033fd981520bc
ArLEquiN64 <ARLEQUIN.prx@gmail.com>
2015-06-06 21:35:26 +0900

    Revert "bug fix"
    This reverts commit 5fab4e7762033d4a49654016420947102f4b9dd2.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+var MongoStore = require('connect-mongo')(express);
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var MongoStore = require('connect-mongo')(express);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
