cbc0fed3d4d4b5d681780523d57cbde46b51c1cb
ArLEquiN64 <ARLEQUIN.prx@gmail.com>
2015-06-07 05:57:01 +0900

    timeline test

---
+++ routes/tweet.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var redis = require('redis');
+require('date-utils');
+
+client = redis.createClient();
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+  client.get("fl-tweets", function(err, reply){
+    if(err){throw err;}
+    else if(reply){
+      var json = {
+        name: req.body.name,
+        tweet: req.body.tweet,
+        time: new Date().toFormat("YYYYMMDDHH24MI");
+      };
+      if(!json.name || !req.body.tweet){res.send("luck params");return;}
+      client.set(req.body.id, JSON.stringify(JSON.parse(reply)+=json), function(err, keys_replies){
+        if(err){throw err;}
+        else{res.send(req.body.name + " tweet.");return;}
+        return;
+      });
+    }
+    else{res.send("error occured.");return;}
+  });
+});
+
+router.get('/', function(req, res, next) {
+  client.get("fl-tweets", function(err, reply){
+    if(err){throw err;}
+    else if(reply){
+      res.send(reply);
+      return;
+    }
+  });
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var redis = require('redis');
+require('date-utils');
 
 client = redis.createClient();
-
-var ws = require('websocket.io');
-var server = ws.listen(8888, function () {
-  console.log('\033[96m Server running at :8888 \033[39m');
-});
-
-// クライアントからの接続イベントを処理
-server.on('connection', function(socket) {
-  // クライアントからのメッセージ受信イベントを処理
-  socket.on('message', function(data) {
-    // 実行時間を追加
-    var data = JSON.parse(data);
-    var d = new Date();
-    data.time = d.getFullYear()  + "-" + (d.getMonth() + 1) + "-" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-    data = JSON.stringify(data);
-    console.log('\033[96m' + data + '\033[39m');
-
-    // 受信したメッセージを全てのクライアントに送信する
-    server.clients.forEach(function(client) {
-      client.send(data);
-    });
-  });
-});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -35,10 +13,23 @@ router.get('/', function(req, res, next) {
 router.get('/:id(\\d+)', function(req, res) {
   client.get(req.params.id, function(err, reply){
     if(err){throw err;}
-    else if(reply){res.send(JSON.stringify(reply));return;}
+    else if(reply){
+      if(req.query.id==req.params.id){
+        createSessionId(req.query.id);
+      }
+      res.send(JSON.stringify(reply));
+      return;
+    }
     else{res.send("invalid id.");return;}
   });
   res.send("error occured");
 });
+
+var createSessionId = function(id) {
+  var dt = new Date();
+  var formatted = dt.toFormat("YYYYMMDDHH24MI");
+  var sessionId = Math.floor(Math.random() * 100000000);
+  //client.set(sessionId, );
+}
 
 module.exports = router;
