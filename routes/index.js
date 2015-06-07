9789b6b1b2da7696fafda4fd51cf05091a895f4c
ArLEquiN64 <ARLEQUIN.prx@gmail.com>
2015-06-07 16:58:13 +0900

    fix

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,7 @@ var checkHash = function() {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'FileLife' });
 });
 
 router.get('/login', function(req, res){
